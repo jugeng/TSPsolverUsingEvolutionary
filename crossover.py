e1406bfc14db27bd4526a20eb072cd3c7a938100
Jugen Gawande <jugengawande@gmail.com>
2020-03-22 18:11:45 +0530

    Added Cycle Crossover v1

--- crossover.py
+++ crossover.py
@@ -5,16 +5,61 @@ def orderedCrossover_SingleCut(parentA, parentB):
     geneCount = len(parentA)
 
     r = random.randint(2, geneCount-1)
-    child = list(parentB[:r])
-    
+    childA = list(parentB[:r])
+    childB = list(parentA[:r])
 
     for gene in np.nditer(parentA, flags=["refs_ok"]):
 
-        if gene in child:
+        if gene in childA:
             continue
         else:
-            child.append(int(gene))
+            childA.append(int(gene))
+
+    for gene in np.nditer(parentB, flags=["refs_ok"]):
+
+        if gene in childB:
+            continue
+        else:
+            childB.append(int(gene))
 
   
-    return child
+    return childA, childB
 
+def cycleCrossover(parentA, parentB):
+    geneCount = len(parentA)
+
+    childA = []
+    childB = []
+
+    childA.append(parentB[0])
+    a = 0
+    while parentA[0] not in childB:
+        a = np.where(parentA == parentB[a])
+        a = np.where(parentA == parentB[a])
+        #print(parentB[a], "->", parentA[a])
+        
+        childB.append(int(parentB[a]))
+
+        a = np.where(parentA == parentB[a])
+
+        if parentB[a] not in childA:
+            childA.append(int(parentB[a]))
+
+    if len(childA) != geneCount:
+        for gene in np.nditer(parentA, flags=["refs_ok"]):
+            if gene in childA:
+                continue
+            else:
+                childA.append(int(gene))
+    
+    if len(childB) != geneCount:
+        for gene in np.nditer(parentB, flags=["refs_ok"]):
+            if gene in childB:
+                continue
+            else:
+                childB.append(int(gene))
+
+    return childA, childB
+
+
+print(cycleCrossover(np.array([3,4,8,2,7,1,6,5]),np.array([4,2,5,1,6,8,3,7])))
