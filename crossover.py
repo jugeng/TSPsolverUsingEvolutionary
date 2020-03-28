20f67ced8aa1740b22547fce96fd6c5eac4bac4c
Jugen Gawande <jugengawande@gmail.com>
2020-03-28 20:26:40 +0530

    Added multipoint ordered crossover operator

--- crossover.py
+++ crossover.py
@@ -79,20 +79,16 @@ def PMS(parentA, parentB):
     
     mapping_a = list(parentB[a:b+1])
     mapping_b = list(parentA[a:b+1])
-
-    print(mapping_a, mapping_b)
-    p = b + 1
-
-
+    
     def inList(val, arrFrom, arrTo):
         res = arrTo[arrFrom.index(val)]
 
         if(res in arrFrom):
             return (inList(res, arrFrom, arrTo))
         else: 
-    
             return res
-
+    
+    p = b + 1
     for i in range(len(parentA)-len(mapping_a)):
      
 
@@ -100,21 +96,15 @@ def PMS(parentA, parentB):
             
             if(parentA[p] in mapping_a):
                 childA.append(inList(parentA[p], mapping_a, mapping_b))
-
             else:
                 childA.append(parentA[p]) 
 
             if(parentB[p] in mapping_b):
                 childB.append(inList(parentB[p], mapping_b, mapping_a))
-
             else:
                 childB.append (parentB[p])
-            
-     
-            
-
+    
         if(p < a):
-
             if(parentA[p] in mapping_a):
                 childA.insert(p, inList(parentA[p], mapping_a, mapping_b) )
             else:
@@ -125,13 +115,63 @@ def PMS(parentA, parentB):
             else:
                 childB.insert(p, parentB[p])
 
-           
-
         if(p == len(parentA)-1):
             p = 0
         else: p +=1
 
     return (childA, childB)
+
+
+def OC_Multi(parentA, parentB):
+        geneCount = len(parentA)
+
+        childA = []
+        childB = []
+
+        a = random.randint(1,geneCount-2)
+        b = random.randint(a+1, geneCount-1)
+    
+        childA = list(parentA[a:b+1])
+        childB = list(parentB[a:b+1])
+
+
+        p = b  #pointer to track parent index
+        c = p+1    #pointer to track child index
+        d = p+1   #pointer to track child index
+
+        for i in range(len(parentA)):
+            p +=1
+            print(p)
+
+            if (p >= len(parentA)-1):p = -1
+            if (c > len(parentA)-1): c = 0
+            if (d > len(parentA)-1): d = 0
+
+            if (c > b):
+                if(parentB[p] not in childA):
+                    childA.append(parentB[p])
+                    c +=1
+            
+
+            if (d > b):
+                if(parentA[p] not in childB):
+                    childB.append(parentA[p])
+                    d +=1
+                  
+
+            if (c < a):
+                if(parentB[p] not in childA):
+                    childA.insert(c, parentB[p])
+                    c +=1
+            
+            if (d < a):
+                if(parentA[p] not in childB):
+                    childB.insert(d, parentA[p])
+                    d +=1
+        
+              
+
+        return(childA, childB)
 
 
 
