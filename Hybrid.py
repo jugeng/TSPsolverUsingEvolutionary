a26a774e939869e3c97a8ef5e319dc9600c08b7e
Jugen Gawande <jugengawande@gmail.com>
2020-04-08 13:33:49 +0530

    working algorithm

--- Hybrid.py
+++ Hybrid.py
@@ -20,6 +20,12 @@ import matplotlib.pyplot as plt
 import sys
 from time import process_time
 
+from multiprocessing import Process, Value, Array
+import threading
+
+T = 0.5
+alpha_temp = 0.9
+
 
 CONFIG = configparser.ConfigParser()                                     
 CONFIG.read('controller.ini')
@@ -35,6 +41,9 @@ loc_multiplier = math.pi / 180
 #Result Store
 minDist = math.inf
 bestRoute = []
+
+tempDistMatx = []
+switch = False
 
 #Data-plotting
 fitness_curve = []
@@ -219,19 +228,15 @@ def calculateFitness():
         if distance < minDist:
             minDist = distance
             bestRoute = np.copy(individual)
-             
     
-    fitness_curve.append(round(minDist  / scale_factor, 2))
     fitnessMatrix = np.asarray(fitness)
     totalFitness = np.sum(fitnessMatrix)
-    fitnessMatrix = np.divide(fitnessMatrix,totalFitness)   #Normalizing the fitness values between [0-1]
+    fitnessMatrix = np.divide(fitnessMatrix,totalFitness)       #Normalizing the fitness values between [0-1]
+
+    fitness_curve.append(round(minDist  / scale_factor, 2))
     generation_fitness.loc[len(generation_fitness)] = fitnessMatrix  
-    nextGenerationMatrix.loc[len(nextGenerationMatrix)] = bestRoute     #Elitism, moving the fittest gene to the new generation as is
-    nextGenerationMatrix.loc[len(nextGenerationMatrix)] = bestRoute 
 
-
-def calculateDistance(loc_1, loc_2):
-    return distanceMatrix.iat[loc_1,loc_2]
+    
 
 
 def mutateChild(gene):
@@ -240,9 +245,13 @@ def mutateChild(gene):
     if r < mutationRate:
         return mutation.RSM(gene)
 
+
 def nextGeneration():
     global nextGenerationMatrix, populationMatrix, genCount, bestRoute
     newGen = []
+
+    nextGenerationMatrix.loc[len(nextGenerationMatrix)] = bestRoute     #Elitism, moving the fittest gene to the new generation as is
+    nextGenerationMatrix.loc[len(nextGenerationMatrix)] = bestRoute 
 
     while (len(newGen) < populationSize-2):
 
@@ -274,45 +283,221 @@ def nextGeneration():
             newGen.append(parentA)
             newGen.append(parentB)
 
-       
+
     nextGenerationMatrix = nextGenerationMatrix.append(newGen[:populationSize-1])
     populationMatrix = nextGenerationMatrix.copy()
     nextGenerationMatrix = nextGenerationMatrix.iloc[0:0]
     calculateFitness()
 
 
+def calculateSolutionFitness(arr):
+    distance = 0
+    for j in range(len(arr)-1):
+        distance += tempDistMatx.iat[arr[j],arr[j+1]]
+
+    return (distance)
+
+
+def reverse(arr, a, b):
+    x = arr[:a]
+    y = arr[a:b+1]
+    z = arr[b+1:]
+
+    w = y[::-1]
+
+    if (b != len(arr)-1):
+        s = calculateSolutionFitness(np.concatenate(([arr[a-1]],y,[arr[b+1]])))
+        s_dash = calculateSolutionFitness(np.concatenate(([arr[a-1]],w,[arr[b+1]])))
+    else:
+        s = calculateSolutionFitness(np.concatenate(([arr[a-1]],y)))
+        s_dash = calculateSolutionFitness(np.concatenate(([arr[a-1]],w)))
+
+    del_e = s_dash - s
+
+    if(del_e < 0):
+        return (np.concatenate((x,w,z)))
+
+    elif(del_e > 0):
+        pr = math.exp((-del_e) / (T) ) 
+        a = random.random()
+
+        if (pr > a):
+            
+            return (np.concatenate((x,w,z)))
+        else: return (arr)
+    else: return(arr)
+
+
+def transport(arr, a, b):
+   
+    x = arr[:a]
+    y = arr[a:b+1]
+    z = arr[b+1:]
+
+    m = arr[a-1:b+1]
+    s = calculateSolutionFitness(m)
+
+    if((b-a) > len(arr)-2):
+        if(b != len(arr)-1):
+            u = random.randint(0,len(z)-1)
+            if (u == 0):
+                s_dash = calculateSolutionFitness(np.concatenate(([arr[a-1]], arr[a:b+1], [z[u]])))
+            else:
+                s_dash = calculateSolutionFitness(np.concatenate(([z[u-1]], arr[a:b+1], [z[u]])))
+        else:
+            u = random.randint(1,len(x)-1)
+            s_dash = calculateSolutionFitness(np.concatenate(([arr[u]], arr[a:b+1], [arr[u+1]])))
+
+    else:
+        return(arr)
+
+    del_e = s_dash - s
+    
+    if(del_e < 0):    
+        return (np.concatenate((x,z[:u],y,z[u:])))
+
+    elif(del_e > 0):
+        pr = math.exp((-del_e) / (T) ) 
+        a = random.random()
+
+        if (pr > a):
+            return (np.concatenate((x,z[:u],y,z[u:])))
+
+        else: return (arr)
+
+    else: return(arr)
+    
+
+def testNeighbor(arr):
+    r = random.random()
+    size = len(arr)
+    a = random.randint(1,size-3)
+    b = random.randint(a+1, size-1)
+    newarr = np.array([])
+
+    if r > 0.5:
+        return (reverse(arr, a, b))
+
+    else:
+        return (transport(arr, a, b))
+    
+
+def SA(arr, val, dist, ret1, ret2):
+    global T, tempDistMatx
+    T = 0.5
+    tempDistMatx = dist
+    accepted = math.inf
+    count = len(arr)
+    accepted = count + 1
+    while(accepted > count / 5):
+        accepted = 0
+        # for i in range(50*len(arr)):
+        for i in range(10*count):
+            new_arr = testNeighbor(arr)
+            if(np.array_equal(new_arr, arr) == False):
+                accepted += 1
+                arr = new_arr
+
+        T *= alpha_temp 
+
+
+
+    ret1.value = calculateSolutionFitness(arr)
+    ret2[:] = arr
+
+
 def GA():
-    global nextGenerationMatrix, populationMatrix, genCount, bestRoute, dead_count, genEvolved,s_t, e_t
+    global nextGenerationMatrix, populationMatrix, genCount, bestRoute, dead_count, genEvolved,s_t, e_t, switch, minDist
 
     counter = 0
     i=0
     end_point = dead_count
-    printProgressBar(0, end_point, prefix = 'Generation:', suffix = 'Complete', length = 40)
+    #printProgressBar(0, end_point, prefix = 'Generation:', suffix = 'Complete', length = 40)
 
     s_t = process_time()
+    
 
-    while(i < genCount):
+    while(1):
         m = minDist
+
+        n = minDist
+        b = bestRoute
+        print(i, ":", minDist / scale_factor)
 
         nextGeneration()
 
         if(minDist == m):
             counter += 1 
+
         else:
             counter = 0
             end_point = i + dead_count 
 
-        if (counter == dead_count or i == genCount-1 ):
+        if(counter == int(dead_count / 4) and switch == False):
+            res = Value('f', minDist, lock=False )
+            res_arr = Array('i', numberOfCities, lock=False )
+            sa = Process(target = SA, args=(b,n, distanceMatrix, res, res_arr))
+            print("start")
+            switch = True
+            sa.start()
+
+
+        if(switch == True):
+            if(sa.is_alive() == False):
+                switch = False
+                print("done")
+                if(res.value < minDist):
+                    print("found better >")
+                    minDist = res.value
+                    bestRoute = res_arr[:]
+                    counter = 0 
+
+        if (counter == dead_count and switch == False):
+
+            #i = genCount
+
+            res = Value('f', minDist, lock=False )
+            res_arr = Array('i', numberOfCities, lock=False )
+            sa = Process(target = SA, args=(b,n, distanceMatrix, res, res_arr))
+            print("start")
+            switch = True
+            sa.start()
+            sa.join()
+            switch = False
+            if(res.value < minDist):
+                minDist = res.val
+                bestRoute = res_arr[:]
+
+        if(counter == dead_count and switch == True):
+            switch = False
+            sa.join()
+            if(res.val < minDist):
+                minDist = res.val
+                bestRoute = b
+                counter = 0 
+            else:
+                pass
+        
+        if(i > genCount):
+            if(sa.is_alive()):
+                sa.join()
+                switch = False
+
+                if(res.value < minDist):
+                    minDist = res.val
+                    bestRoute = res_arr[:]
+                    i = i - dead_count
+
             genEvolved = len(fitness_curve)
             logger.info("\nGENERATIONS EVOLVED={gen}".format(gen=str(genEvolved)))
             e_t = process_time()
             logger.info("CPU execution time: {}".format(e_t-s_t))
             break 
-        i+=1 
-        printProgressBar(i, end_point , prefix = 'Generation:', suffix = 'Evolved', length = 40)
-    #logger.info("GENERATIONS EVOLVED={gen}\n".format(gen=i))   #Enable if a stopping condition is maintained  
-
-
+        
+        else:
+            i+=1 
+        #printProgressBar(i, end_point , prefix = 'Generation:', suffix = 'Evolved', length = 40)
+    
 
 #Graphing
 def graphing(): 
@@ -394,11 +579,10 @@ def graphing():
     logger.info("Fitness Curve exported to logs\nFile name: {}".format(hjhsda) )
     #plt.show()   #To view graph after generating 
 
-
 #Data Logging
-def logging_setup():
+def loggingSetup():
     global logger
-
+    logger = logging.getLogger('tsp_hybrid')
     fname = "./logs/test_log/TSP_GA_" + datetime.now().strftime("%d-%m-%y %H_%M") + "_" + data +"_"+ str(populationSize) + "_" + str(mutationRate) + ".log"
 
     logger.setLevel(logging.INFO)
@@ -419,50 +603,7 @@ def logging_setup():
     logger.info("\nPOPULATION SIZE={pop} \nMUTATION RATE={mut} \nDATASET SELECTED={name}".format(pop =populationSize, mut = mutationRate, name = data))
 
 
-#Controller Variables
-data = CONFIG['DATASET']['FILE_NAME']
-data_type_flag = CONFIG.getint('DATASET', 'DATASET_TYPE')
-populationSize = CONFIG.getint('GENETIC', 'POP_SIZE')
-if (populationSize < 1):
-    logger.warning("Population size not enough")
-    sys.exit()
-mutationRate = CONFIG.getfloat('GENETIC', 'MUTATION_RATE')
-genCount = CONFIG.getint('GENETIC', 'GEN_COUNT')
-dead_count = CONFIG.getint('GENETIC', 'DEAD_COUNTER')
-cx_opt = CONFIG['OPERATOR']['CROSSOVER_OPERATOR']
-set_debug = CONFIG.getboolean('DEBUG', 'LOG_FILE')
-data_cordinate = CONFIG.getboolean('DATASET','CONTAINS_COORDINATES')
-
-
-logger = logging.getLogger('tsp_ga')
-logging_setup()
-
-data_fname = "./dataset/" + data + ".txt"   #Select data file to use
-
-if data_type_flag == 0:
-    addCity_using_coords()
-
-else:
-    addCity_using_dist()
-
-#Initialize pandas dataframes
-generation_fitness = pd.DataFrame(columns = np.arange(populationSize))
-populationMatrix = pd.DataFrame(columns=np.arange(numberOfCities))
-nextGenerationMatrix = pd.DataFrame(columns=np.arange(numberOfCities))
-
-
-#Run Genetic Algorithm
-generateInitPop()
-GA()
-
-
-logger.info("MINIMAL DISTANCE={}".format(minDist / scale_factor))
-logger.info("BEST ROUTE FOUND={}".format(bestRoute))
-logger.info("\nAlgorithm Completed Successfully.")
-logger.info("FITNESS CURVE:\n{}".format(fitness_curve[:genEvolved]))   #Will fail if all generations are exhausted
-
-
-if(set_debug == True):
+def outputRecord():
     with open("./logs/visualize_data.txt", "w") as f:
         f.write(" ".join(str(item) for item in fitness_curve))
         f.write("\n")
@@ -488,4 +629,58 @@ if(set_debug == True):
 
     graphing()
 
-logger.info("All done")
+#Controller Variables
+def initializeAlgorithm():
+    global data, data_type_flag, populationSize, mutationRate, genCount, dead_count, cx_opt, set_debug, data_cordinate, data_fname
+
+    data = CONFIG['DATASET']['FILE_NAME']
+    data_type_flag = CONFIG.getint('DATASET', 'DATASET_TYPE')
+    populationSize = CONFIG.getint('GENETIC', 'POP_SIZE')
+    if (populationSize < 1):
+        logger.warning("Population size not enough")
+        sys.exit()
+    mutationRate = CONFIG.getfloat('GENETIC', 'MUTATION_RATE')
+    genCount = CONFIG.getint('GENETIC', 'GEN_COUNT')
+    dead_count = CONFIG.getint('GENETIC', 'DEAD_COUNTER')
+    cx_opt = CONFIG['OPERATOR']['CROSSOVER_OPERATOR']
+    set_debug = CONFIG.getboolean('DEBUG', 'LOG_FILE')
+    data_cordinate = CONFIG.getboolean('DATASET','CONTAINS_COORDINATES')
+
+    data_fname = "./dataset/" + data + ".txt"
+
+
+
+
+if __name__ == '__main__':
+
+    initializeAlgorithm()
+    loggingSetup()
+
+
+    if data_type_flag == 0:
+        addCity_using_coords()
+
+    else:
+        addCity_using_dist()
+
+
+    #Initialize pandas dataframes
+    generation_fitness = pd.DataFrame(columns = np.arange(populationSize))
+    populationMatrix = pd.DataFrame(columns=np.arange(numberOfCities))
+    nextGenerationMatrix = pd.DataFrame(columns=np.arange(numberOfCities))
+
+    #Run Genetic Algorithm
+    generateInitPop()
+    GA()
+
+
+    logger.info("MINIMAL DISTANCE={}".format(minDist / scale_factor))
+    logger.info("BEST ROUTE FOUND={}".format(bestRoute))
+    logger.info("\nAlgorithm Completed Successfully.")
+    #logger.info("FITNESS CURVE:\n{}".format(fitness_curve[:genEvolved]))   #Will fail if all generations are exhausted
+
+
+    if(set_debug == True):
+        outputRecord()
+
+    logger.info("All done")
